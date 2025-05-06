ba988548606f4c8bad3f4b944230409986a2a1ef
Paweł Lidwin <lidwinpawel@gmail.com>
2025-05-07 00:13:49 +0200

    fix(xdelta3): use simpler way of getting type sizes

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@
 // directly at the moment
 
 fn main() {
-    let mut defs = vec![
+    let defs = vec![
         ("EXTERNAL_COMPRESSION".to_string(), "1".to_string()),
         ("SECONDARY_DJW".to_string(), "0".to_string()),
         ("SECONDARY_FGK".to_string(), "0".to_string()),
@@ -11,17 +11,11 @@ fn main() {
         ("SHELL_TESTS".to_string(), "0".to_string()),
         ("XD3_ENCODER".to_string(), "0".to_string()),
         ("XD3_USE_LARGEFILE64".to_string(), "1".to_string()),
+        ("SIZEOF_SIZE_T".to_string(), std::mem::size_of::<usize>().to_string()),
+        ("SIZEOF_UNSIGNED_INT".to_string(), std::mem::size_of::<std::os::raw::c_uint>().to_string()),
+        ("SIZEOF_UNSIGNED_LONG".to_string(), std::mem::size_of::<std::os::raw::c_ulong>().to_string()),
+        ("SIZEOF_UNSIGNED_LONG_LONG".to_string(), std::mem::size_of::<std::os::raw::c_ulonglong>().to_string()),
     ];
-
-    for i in &[
-        "size_t",
-        "unsigned int",
-        "unsigned long",
-        "unsigned long long",
-    ] {
-        let def_name = format!("SIZEOF_{}", i.to_uppercase().replace(' ', "_"));
-        defs.push((def_name, check_native_size(i)));
-    }
 
     {
         let mut build = cc::Build::new();
@@ -53,51 +47,4 @@ fn main() {
             )
             .expect("Failed to write bindings");
     }
-}
-
-fn check_native_size(name: &str) -> String {
-    use rand::Rng;
-    use std::fs::remove_file;
-    use std::io::Write;
-
-    let builder = cc::Build::new();
-    let out_dir = std::env::var("OUT_DIR").unwrap();
-    let compiler = builder.get_compiler();
-    let mut compile = std::process::Command::new(compiler.path().as_os_str());
-    let test_code = format!("#include <stdint.h>\n#include <stdio.h>\nint main() {{printf(\"%lu\", sizeof({})); return 0;}}\n", name);
-    // didn't use tempfile since tempfile was having issues on Windows
-    let mut rng = rand::thread_rng();
-    let test_binary_fn = format!("{}/test-{}", out_dir, rng.gen::<i32>());
-
-    #[cfg(windows)]
-    let test_binary_fn = format!("{}.exe", test_binary_fn);
-
-    let test_source_fn = format!("{}/test-{:x}.c", out_dir, rng.gen::<i32>());
-    let mut test_source =
-        std::fs::File::create(&test_source_fn).expect("Error creating test compile files");
-
-    compile.args(compiler.args()).current_dir(out_dir);
-    if compiler.is_like_msvc() {
-        compile.args([&test_source_fn, &format!("/Fe{}", test_binary_fn)]);
-    } else {
-        compile.args([&test_source_fn, "-o", &test_binary_fn]);
-    }
-    test_source
-        .write_all(test_code.as_bytes())
-        .expect("Error writing test compile files");
-    drop(test_source); // close the source file, otherwise there will be problems on Windows
-    for (a, b) in compiler.env().iter() {
-        compile.env(a, b);
-    }
-    compile.output().expect("Error compiling test source");
-    remove_file(test_source_fn).ok();
-
-    compile = std::process::Command::new(&test_binary_fn);
-    let output = compile
-        .output()
-        .expect("Error executing test binary")
-        .stdout;
-    let output = String::from_utf8(output).expect("Error converting Unicode sequence");
-    remove_file(test_binary_fn).ok();
-    output
 }
